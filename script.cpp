42eceee5404cc27682adb894349b1a0523de656c
Stamatis <stamatis.stamatelopoulos1@gmail.com>
2022-08-11 20:06:53 +0300

    test_commit

--- script.cpp
+++ script.cpp
@@ -1,6 +1,17 @@
+#include <tinynurbs/tinynurbs.h>
+#include <glm/glm.hpp>
 #include <iostream>
 
 int main() {
+	
+	tinynurbs::Curve<float> crv; // Planar curve using float32
+	crv.control_points = {glm::vec3(-1, 0, 0), // std::vector of 3D points
+                      	glm::vec3(0, 1, 0),
+                      	glm::vec3(1, 0, 0)
+                     	};
+	crv.knots = {0, 0, 0, 1, 1, 1}; // std::vector of floats
+	crv.degree = 2;
+
 	std::cout << "Hello World!" << std::endl;
 	
 	return 1;
