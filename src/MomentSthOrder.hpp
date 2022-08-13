be7b4d730e2bdf81329358ce9a94148d5fa82b50
Stamatis <stamatis.stamatelopoulos1@gmail.com>
2022-08-13 19:44:16 +0300

    moment and simple triangluation files

---
+++ src/MomentSthOrder.hpp
@@ -0,0 +1,28 @@
+#ifndef MOMENTSTHORDER_HPP
+#define MOMENTSTHORDER_HPP
+
+double MomentSthOrder(
+	std::vector<std::vector<std::vector<double>>> triangles,
+	int p,
+	int q,
+	int r,
+	bool is_translation_invariant,
+	bool is_scaling_invariant
+	);
+
+double MomentSthOrder(
+	std::vector<std::vector<std::vector<double>>> triangles,
+	int p,
+	int q,
+	int r,
+	bool is_translation_invariant,
+	bool is_scaling_invariant
+	)
+{
+	/*
+		
+	*/
+}
+
+
+#endif // MOMENTSTHORDER_HPP
