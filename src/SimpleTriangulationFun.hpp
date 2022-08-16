a7a3ae3f1fb1d27ef092ce9ee56d40bf688fdcec
Stamatis <stamatis.stamatelopoulos1@gmail.com>
2022-08-16 10:25:00 +0300

    PlanarTriangulation() class compatibility

--- src/SimpleTriangulationFun.hpp
+++ src/SimpleTriangulationFun.hpp
@@ -118,6 +118,7 @@ namespace SimpleTriangulation{
 				- std::vector<double> Y
 					Second side of rectangle: [Y.at(0), Y.at(1)]
 				- int N
+					N > 0
 					A triangulation consisting of 2*ceil(sqrt(N/2))^2 rectangles is produced
 			
 			Output:
@@ -132,42 +133,102 @@ namespace SimpleTriangulation{
 		
 		int M = std::ceil(std::sqrt(N/2)); //number of subintervals in X,Y
 		
-		std::vector<double> X_discrete(M+1,0);
-		std::vector<double> Y_discrete(M+1,0);
+		std::vector<std::vector<double>> nodes((M+1)*(M+1), std::vector<double>(2,0));// nodes for Triangulation class
+		// nodes.at(k*(M+1)) through nodes.at(k*(M+1) + M) for k = 0,...,M are the points in the kth row of the planar discretization
+		
+		int count = 0;// counts nodes
 		
 		for (int i = 0; i < (M+1); i++){
-			X_discrete.at(i) = X.at(0) + (X.at(1)-X.at(0))*i/M;
-			Y_discrete.at(i) = Y.at(0) + (Y.at(1)-Y.at(0))*i/M;
+			for (int j = 0; j < (M+1); j++){
+				nodes.at(count).at(0) = X.at(0) + (X.at(1)-X.at(0))*j/M;
+				nodes.at(count).at(1) = Y.at(0) + (Y.at(1)-Y.at(0))*i/M;
+				count++;
+			}
 		}
 		
 		/* Constructing Triangles */
 		
-		std::vector<std::vector<std::vector<double>>> triangles(2*M*M,std::vector<std::vector<double>>(3,std::vector<double>(2,0))); //triangles initialized to all zeros
+		std::vector<std::vector<int>> faces(2*M*M,std::vector<int>(6,0));// faces for Triangulation class
+		// faces.at(k*2*M) through faces.at(k*2*M + 2*M - 1) for k = 0,...,(M-1) are the triangles in the kth row of subrectangles
+		// faces.at(k*2*M + 2*j) for j = 0,...,M is the lower right triangle of the jth subrectangle in the kth row of subrectangles
+		// faces.at(k*2*M + 2*j + 1) for j = 0,...,M is the upper left triangle of the jth subrectangle in the kth row of subrectangles
+				
+		//Indexes of triangles in faces, relative to  current triangle
+		int down_upper_left = 0;// upper left triangle in subrectangle down
+		int top_lower_right = 0;// etc.
+		int right_upper_left = 0;
+		int left_lower_right = 0;
 		
-		int count = 0;// counts rectangles
+		count = 0;// counts faces
+		
 		for (int i = 0; i < M; i++){
 			for (int j = 0; j < M; j++){
 				// (i,j) denotes the (i,j)th discrete subrectangle of X \times Y
 				
+				down_upper_left = (i-1)*2*M + 2*j + 1;
+				top_lower_right = (i+1)*2*M + 2*j;
+				right_upper_left = i*2*M + 2*(j+1) + 1;
+				left_lower_right = i*2*M + 2*(j-1);
+				
 				/* lower right triangle */
 				
-				triangles.at(count).at(0) = std::vector<double> {X_discrete.at(i), Y_discrete.at(j)};
-				triangles.at(count).at(1) = std::vector<double> {X_discrete.at(i+1), Y_discrete.at(j)};
-				triangles.at(count).at(2) = std::vector<double> {X_discrete.at(i+1), Y_discrete.at(j+1)};
+				faces.at(count).at(0) = i*(M+1) + j;// lower left vertex
+				faces.at(count).at(1) = i*(M+1) + j + 1;// lower right vertex
+				faces.at(count).at(2) = (i+1)*(M+1) + j + 1;// upper right vertex
+				
+				faces.at(count).at(4) = count + 1;// oppposite triangle to low right corner is the other half of the subrectangle
+				
+				// Determining neighbours (see Triangulation class definition)
+				if (i == 0 && j ==0) {// bottom left corner of XY
+					faces.at(count).at(3) = right_upper_left;
+					faces.at(count).at(5) = count;// Triang. class convention (points to itself)
+				}
+				else if (i == 0 && j == (M-1)){// bottom right corner of XY
+					faces.at(count).at(3) = count;
+					faces.at(count).at(5) = count;
+				}
+				else if (i == (M-1) && j == (M-1)){// upper right corner of XY
+					faces.at(count).at(3) = count;
+					faces.at(count).at(5) = down_upper_left;
+				}
+				else{// has subrectangles to the right and down
+					faces.at(count).at(3) = right_upper_left;
+					faces.at(count).at(5) = down_upper_left;
+				}
 				
 				count++;
 				
 				/* upper left triangle */
 				
-				triangles.at(count).at(0) = std::vector<double> {X_discrete.at(i), Y_discrete.at(j)};
-				triangles.at(count).at(1) = std::vector<double> {X_discrete.at(i), Y_discrete.at(j+1)};
-				triangles.at(count).at(2) = std::vector<double> {X_discrete.at(i+1), Y_discrete.at(j+1)};
+				faces.at(count).at(0) = i*(M+1) + j;// lower left vertex
+				faces.at(count).at(1) = (i+1)*(M+1) + j;// upper left vertex
+				faces.at(count).at(2) = (i+1)*(M+1) + j + 1;// upper right vertex
 				
+				faces.at(count).at(4) = count - 1;// oppposite triangle to upper left corner is the other half of the subrectangle
+						
+				// Determining neighbours (see Triangulation class definition)
+				if (i == 0 && j ==0) {// bottom left corner of XY
+					faces.at(count).at(3) = top_lower_right;
+					faces.at(count).at(5) = count;// Triang. class convention (points to itself)
+				}
+				else if (i == (M-1) && j == 0){// upper left corner of XY
+					faces.at(count).at(3) = count;
+					faces.at(count).at(5) = count;
+				}
+				else if (i == (M-1) && j == (M-1)){// upper right corner of XY
+					faces.at(count).at(3) = count;
+					faces.at(count).at(5) = left_lower_right;
+				}
+				else{// has subrectangles to the left and up
+					faces.at(count).at(3) = top_lower_right;
+					faces.at(count).at(5) = left_lower_right;
+				}
+								
 				count++;
 			}
 		}
 		
-		return(Triangulation<std::vector<double>>());
+		return(Triangulation<std::vector<double>>(nodes,faces));
 		
 	}// PlanarTriangulation()
 	
